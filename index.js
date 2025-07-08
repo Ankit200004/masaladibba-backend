7318b15e2ad4a0fa99866ef739f0524f4b635292
Ankit200004 <Ankit200004.modiankit@201gmail.com>
2025-07-08 18:02:11 +0530

    Setup backend with MongoDB and Express

--- index.js
+++ index.js
@@ -18,6 +18,7 @@ app.use(cors({
   origin: "https://masaladibba.com",
   credentials: true,
 }));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
